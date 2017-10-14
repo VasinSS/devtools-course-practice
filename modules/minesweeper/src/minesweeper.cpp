6e986cccd6ca31695ad679e73e6f7c4447df120a
StasSV <stascnfcstascnfc@mail.ru>
2017-10-14 21:14:12 +0300

    :|

--- modules/minesweeper/src/minesweeper.cpp
+++ modules/minesweeper/src/minesweeper.cpp
@@ -72,8 +72,10 @@ char minesweeper::cell_is_mine(char ix, char iy) {
 char minesweeper::get_area_mine_count(char ix, char iy) {
   char area_mine = 0;
   for (int i = 0; i < mine_count; i++) {
-    if (std::abs(static_cast<int>(x_mine_pos[i] - static_cast<int>(ix))) <= 1) {
-      if (std::abs(static_cast<int>(y_mine_pos[i] - static_cast<int>(iy))) <= 1) {
+    if (fabs(static_cast<int>(x_mine_pos[i]
+                 - static_cast<int>(ix))) <= 1) {
+      if (fabs(static_cast<int>(y_mine_pos[i]
+                  - static_cast<int>(iy))) <= 1) {
         area_mine = area_mine + 1;
       }
     }
